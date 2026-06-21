1af8516ab83c451f82b44a8e1bc27bdf8a545fec
Avengers0327 <31meha01@mahwah.k12.nj.us>
2026-06-21 12:33:06 -0400

    Update home.py

--- pages/home.py
+++ pages/home.py
@@ -2,6 +2,21 @@ import streamlit as st
 
 banner_url = "https://upload.wikimedia.org/wikipedia/commons/9/9e/Radia_Perlman_%2820175369862%29.jpg"
 
+st.set_page_config(layout="wide")
+
+pg = st.navigation([
+    st.Page("pages/home.py", title="Home", icon=None),
+    st.Page("pages/thesis.py", title="Thesis", icon=None),
+    st.Page("pages/early.py", title="The Early Internet", icon=None),
+    st.Page("pages/looping.py", title="Infinite Looping", icon=None),
+    st.Page("pages/stp.py", title="Spanning Tree Protocol", icon=None),
+    st.Page("pages/legacy.py", title="Legacy", icon=None),
+    st.Page("pages/cited.py", title="Works Cited", icon=None),
+], position="top")
+
+pg.run()
+
+
 st.markdown("""
 <style>
 
